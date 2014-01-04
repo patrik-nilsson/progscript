a46808e68e46324adefc93155a75255bf4c88cef
patrik-nilsson <patriknilson@hotmail.com>
2014-01-04 17:46:01 +0100

    Jan 4 17:46:01

---
+++ Lab7/ovning4-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+zenity --list \
+  --title="Choose the Bugs You Wish to View" \
+  --column="Bug Number" \
+  "google-chrome" \
+  "firefox" 
+Cho=`cat /tmp/menu`
+`cat /tmp/menu`
+exit 0
